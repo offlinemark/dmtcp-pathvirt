d2c8d2a3e31b632b34439c1cdcf2ccfe9110245f
Mark Mossberg <mark.mossberg@gmail.com>
2015-07-31 11:06:21 -0400

    Add support for DMTCP_PATH_PREFIX env var
    If DMTCP_PATH_PREFIX is set on launch and restart, all open syscalls
    with the launch value as a prefix will be replaced with the restart
    value

--- pathvirt.c
+++ pathvirt.c
@@ -7,40 +7,35 @@
 #include <stdio.h>
 #include <sys/time.h>
 #include <string.h>
+#include <stdlib.h>
 #include "dmtcp.h"
 
-// always include trailing /
-const char* oldprefix="/home/markmoss/dmtcp/test/plugin/pathvirt/slot5/";
-const char* newprefix="/home/markmoss/dmtcp/test/plugin/pathvirt/misc/slot7/";
+#define ENV_DPP "DMTCP_PATH_PREFIX"
 
-void print_time() {
-  struct timeval val;
-  gettimeofday(&val, NULL);
-  printf("%ld %ld", (long)val.tv_sec, (long)val.tv_usec);
-}
+/* paths should only be swapped on restarts (not on initial run), so this flag */
+/* is set on restart */
+static int should_swap;
 
-/* unsigned int sleep(unsigned int seconds) { */
-/*   printf("sleep1: "); print_time(); printf(" ... "); */
-/*   unsigned int result = NEXT_FNC(sleep)(seconds); */
-/*   print_time(); printf("\n"); */
-
-/*   return result; */
-/* } */
+// NOTE: DMTCP_PATH_PREFIX env variables cannot exceed 512 characters in length
+// TODO
+static char old_path_prefix[512];
+static char new_path_prefix[512];
 
 static int startswith(const char *target, const char *prefix) {
     // invert strncmp ret to return true if startswith and false else
     return !strncmp(target, prefix, strlen(prefix));
 }
 
-
 int open(const char *path, int oflag, mode_t mode) {
-    printf("WRAPPED OPEN2!\n");
-    if (startswith(path, oldprefix)) {
-        puts("old path prefix detected. swapping");
-        size_t newpathsize = (strlen(path) - strlen(oldprefix)) + strlen(newprefix);
+    if (should_swap && startswith(path, old_path_prefix)) {
+        // plus 1 is for safety slash we include between new_path_prefix a
+        size_t newpathsize = (strlen(path) - strlen(old_path_prefix)) + strlen(new_path_prefix) + 1;
         char newpath[newpathsize + 1];
-        snprintf(newpath, sizeof newpath, "%s%s", newprefix,
-                 path + strlen(oldprefix));
+        snprintf(newpath, sizeof newpath, "%s/%s", new_path_prefix,
+                 path + strlen(old_path_prefix));
+        puts(newpath);
+        puts(old_path_prefix);
+        printf("%p\n", old_path_prefix);
         return NEXT_FNC(open)(newpath, oflag, mode);
     } else {
         return NEXT_FNC(open)(path, oflag, mode);
@@ -49,19 +44,62 @@ int open(const char *path, int oflag, mode_t mode) {
 
 void dmtcp_event_hook(DmtcpEvent_t event, DmtcpEventData_t *data)
 {
-  /* NOTE:  See warning in plugin/README about calls to printf here. */
-  switch (event) {
-  case DMTCP_EVENT_WRITE_CKPT:
-    printf("\n*** The plugin %s is being called before checkpointing. ***\n",
-	   __FILE__);
-    break;
-  case DMTCP_EVENT_RESUME:
-    printf("*** The plugin %s has now been checkpointed. ***\n", __FILE__);
-    break;
-  default:
-    ;
-  }
+    /* NOTE:  See warning in plugin/README about calls to printf here. */
+    switch (event) {
+    case DMTCP_EVENT_INIT:
+    {
+        /* On init, check if they've specified paths to virtualize via
+           DMTCP_PATH_PREFIX env */
+        char *old_env = getenv(ENV_DPP);
+        if (old_env) {
+            /* if so, save it to buffer */
+            strncpy(old_path_prefix, old_env, sizeof old_path_prefix);
+            old_path_prefix[strlen(old_env)] = 0;
+        }
 
-  /* Call this next line in order to pass DMTCP events to later plugins. */
-  DMTCP_NEXT_EVENT_HOOK(event, data);
+        break;
+    }
+    case DMTCP_EVENT_RESTART:
+    {
+        /* necessary since we don't know how many bytes dmtcp_get_restart_env
+           will write */
+        memset(new_path_prefix, 0, sizeof new_path_prefix);
+
+        /* Try to get the value of ENV_DPP from new environment variables,
+         * passed in on restart */
+        int ret = dmtcp_get_restart_env(ENV_DPP, new_path_prefix,
+                                        sizeof(new_path_prefix) - 1);
+        printf("%d\n", ret);
+        if (ret == -1) {
+            /* env var did not exist. no new prefix given, so do nothing */
+            break;
+        } else if (ret == -2) {
+            /* need to allocate more memory and retry */
+        }
+
+        /* check if an initial DMTCP_PATH_PREFIX was even supplied
+         * (old_path_prefix is initialized to zeros on start, and would only
+         * contain something if something had been written there */
+        if (*old_path_prefix)
+            should_swap = 1;
+
+        /* else */
+        /*     puts("no old path given!"); */
+
+        break;
+    }
+
+    case DMTCP_EVENT_WRITE_CKPT:
+        printf("\n*** The plugin %s is being called before checkpointing. ***\n",
+           __FILE__);
+        break;
+    case DMTCP_EVENT_RESUME:
+        printf("*** The plugin %s has now been checkpointed. ***\n", __FILE__);
+        break;
+    default:
+    ;
+    }
+
+    /* Call this next line in order to pass DMTCP events to later plugins. */
+    DMTCP_NEXT_EVENT_HOOK(event, data);
 }
